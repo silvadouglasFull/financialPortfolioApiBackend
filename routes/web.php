ed31f8c7b79224000e56869249b9dedb9262f6fb
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-07-15 10:52:54 -0300

    feat: add all routes from web on web routers

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+require __DIR__ . '/web/admin.php';
+require __DIR__ . '/web/auth.php';
+require __DIR__ . '/web/register.php';
+require __DIR__ . '/web/transaction.php';
 Route::get('/', function () {
     return view('welcome');
 });
