098076bff6f7163d478f14210b25d1bb5585468b
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-07-15 10:19:42 -0300

    fixe: changed path files of controllers

--- routes/transactions.reverse.php
+++ routes/transactions.reverse.php
@@ -1,9 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\ReversalController; // Importe o ReversalController
 
 /*
