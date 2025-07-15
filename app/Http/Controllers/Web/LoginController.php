182c4fdc36be46df520a6244faf42042d6f4da74
silvadouglasFull <suportedouglaspostopratico@gmail.com>
2025-07-15 10:33:15 -0300

    feat: add new controller to show form login on view

---
+++ app/Http/Controllers/Web/LoginController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use Illuminate\View\View; // Importar a classe View
+
+/**
+ * Class LoginController
+ *
+ * Controlador responsável por exibir a página de login para usuários via web.
+ */
+class LoginController extends Controller
+{
+    /**
+     * Exibe o formulário de login.
+     *
+     * @return View
+     */
+    public function showLoginForm(): View
+    {
+        return view('auth.login'); // Retorna a view 'auth/login.blade.php'
+    }
+}
